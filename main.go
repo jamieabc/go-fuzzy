4c46bb420047bf0f4155936a145e0e90aded5481
Aaron Chang <aaron.chang@quadas.com>
2018-09-18 12:11:16 +0800

    refactor code, use object for manipulation

--- main.go
+++ main.go
@@ -7,14 +7,21 @@ import (
 	"io/ioutil"
 	"local/random"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 )
 
+type dataGeneration interface {
+	justifyData(methodName string)
+	genRandomData()
+	sampleData()
+}
+
 type rpcFormat struct {
-	ID     int              `json:"id,omitempty"`
-	Method string           `json:"method,omitempty"`
-	Params provenanceFormat `json:"params,omitempty"`
+	ID     string             `json:"id,omitempty"`
+	Method string             `json:"method,omitempty"`
+	Params []provenanceFormat `json:"params,omitempty"`
 }
 
 type provenanceFormat struct {
@@ -23,34 +30,62 @@ type provenanceFormat struct {
 }
 
 const (
-	host string = "127.0.0.1"
-	port string = "2131"
-	path string = "/bitmarkd/rpc"
-	url  string = "https://" + host + ":" + port + path
+	scheme  string = "https"
+	host    string = "127.0.0.1:2131"
+	rpcPath string = "/bitmarkd/rpc"
 )
 
+var rpcURL = url.URL{
+	Scheme: scheme,
+	Host:   host,
+	Path:   rpcPath,
+}
+
+func (rpc *rpcFormat) justifyData(methodName string) {
+	rpc.Method = methodName
+	rpc.ID = "1"
+	for _, p := range rpc.Params {
+		p.Count = 20
+	}
+}
+
 // genRandomData generates random data fits specific interface
-func genRandomData(params interface{}, methodName string) (io.Reader, error) {
-	fmt.Println("params: ", params)
+func (rpc *rpcFormat) genRandomData() {
 	r := random.New()
-	r.Fuzz(params)
+	r.Fuzz(rpc)
+}
+
+func generateJSON(params dataGeneration, methodName string) (io.Reader, error) {
+	params.genRandomData()
+	params.justifyData(methodName)
+
+	// keep for test
+	// params.sampleData()
 
 	paramsJSON, err := json.Marshal(params)
 	if err != nil {
 		return nil, err
 	}
 	paramsStr := string(paramsJSON)
+	fmt.Println("post body: ", paramsStr)
 	return strings.NewReader(paramsStr), nil
 }
 
 // sampleData generates correct data
-func sampleData() io.Reader {
-	return strings.NewReader(`{"id":"1","method":"Bitmark.Provenance","params":[{"count":20,"txId":"2dc8770718b01f0205ad991bfb4c052f02677cff60e65d596e890cb6ed82c861"}]}`)
+func (rpc *rpcFormat) sampleData() {
+	rpc.ID = "1"
+	rpc.Method = "Bitmark.Provenance"
+	rpc.Params = []provenanceFormat{
+		provenanceFormat{
+			Count: 20,
+			TxID:  "2dc8770718b01f0205ad991bfb4c052f02677cff60e65d596e890cb6ed82c861",
+		},
+	}
 }
 
 // send http request
 func sendRequest(reqBody io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest("POST", url, reqBody)
+	req, err := http.NewRequest("POST", rpcURL.String(), reqBody)
 	if err != nil {
 		fmt.Printf("error message: %s\n", err)
 		return nil, err
@@ -58,7 +93,9 @@ func sendRequest(reqBody io.Reader) (*http.Response, error) {
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	resp, err := http.DefaultClient.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
 	if err != nil {
 		fmt.Printf("error message: %s\n", err)
 		return nil, err
@@ -71,7 +108,7 @@ func main() {
 	params := &rpcFormat{}
 
 	for true {
-		body, err := genRandomData(params, "Bitmark.Provenance")
+		body, err := generateJSON(params, "Bitmark.Provenance")
 
 		if nil != err {
 			fmt.Println("Error message: ", err)
@@ -90,7 +127,7 @@ func main() {
 			fmt.Printf("error message: %s\n", err)
 			return
 		}
-		fmt.Printf("response: %s\n\n", response)
+		fmt.Printf("%s\n\n", string(response))
 
 		// wait a bit
 		time.Sleep(50 * time.Millisecond)
