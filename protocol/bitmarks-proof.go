c767a835048211000453d09e70b94e7b269a041b
Aaron Chang <aaron.chang@quadas.com>
2018-09-19 10:41:54 +0800

    Add interface for Bitmarks.Proof

---
+++ protocol/bitmarks-proof.go
@@ -0,0 +1,37 @@
+package protocol
+
+import "local/random"
+
+type BitmarksProofRpc struct {
+	ID     string          `json:"id,omitempty"`
+	Method string          `json:"method,omitempty"`
+	Params []BitmarksProof `json:"params,omitempty"`
+}
+
+type BitmarksProof struct {
+	PayId string `json:"payId"`
+	Nonce string `json:"nonce"`
+}
+
+func (rpc *BitmarksProofRpc) JustifyData() {
+	rpc.Method = "Bitmarks.Proof"
+	rpc.ID = "1"
+}
+
+// genRandomData generates random data fits specific interface
+func (rpc *BitmarksProofRpc) GenRandomData() {
+	r := random.New()
+	r.Fuzz(rpc)
+}
+
+// sampleData generates correct data
+func (rpc *BitmarksProofRpc) SampleData() {
+	rpc.ID = "1"
+	rpc.Method = "Bitmarks.Proof"
+	rpc.Params = []BitmarksProof{
+		BitmarksProof{
+			PayId: "e219ffe8021190ea472baa147b05b2fbfb79818ab6eb267a037b7744f3b89a5966723180e1367ce7e172369d7432a658",
+			Nonce: "c114fa516a98c3de",
+		},
+	}
+}
