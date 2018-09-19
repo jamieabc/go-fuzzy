f6e910eb940578a6664282ceaf5bfbfce4bb0691
Aaron Chang <aaron.chang@quadas.com>
2018-09-19 10:42:11 +0800

    Add interface for Transaction.Status

---
+++ protocol/transaction-status.go
@@ -0,0 +1,35 @@
+package protocol
+
+import "local/random"
+
+type TransactionStatusRpc struct {
+	ID     string              `json:"id,omitempty"`
+	Method string              `json:"method,omitempty"`
+	Params []TransactionStatus `json:"params,omitempty"`
+}
+
+type TransactionStatus struct {
+	TxID string `json:"TxId,omitempty"`
+}
+
+func (rpc *TransactionStatusRpc) JustifyData() {
+	rpc.Method = "Transaction.Status"
+	rpc.ID = "1"
+}
+
+// genRandomData generates random data fits specific interface
+func (rpc *TransactionStatusRpc) GenRandomData() {
+	r := random.New()
+	r.Fuzz(rpc)
+}
+
+// sampleData generates correct data
+func (rpc *TransactionStatusRpc) SampleData() {
+	rpc.ID = "1"
+	rpc.Method = "Transaction.Status"
+	rpc.Params = []TransactionStatus{
+		TransactionStatus{
+			TxID: "2dc8770718b01f0205ad991bfb4c052f02677cff60e65d596e890cb6ed82c861",
+		},
+	}
+}
